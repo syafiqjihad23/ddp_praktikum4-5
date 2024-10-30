f75e118cacfa7a4bc8415457e9d8672ff4fd30c2
syafiqjihad23 <160695792+syafiqjihad23@users.noreply.github.com>
2024-10-30 09:34:28 +0700

    Add files via upload

---
+++ tugas3.py
@@ -0,0 +1,10 @@
+usia = int(input('Masukan usia Anda\n= '))
+
+if usia >=18 :
+    print("Kamu sudah dewasa")
+elif usia >=13 & usia <=17 :
+    print("Kamu saat ini sedang memasuki masa remaja")
+elif usia <18 :
+    print("Kamu masih bocil")
+else:
+    print("Kamu tidak diketahui")    
